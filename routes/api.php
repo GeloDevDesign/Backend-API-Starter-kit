d45aef0f7d2ed1449d5be0920714edef1c463e4d
GeloDevDesign <abserenuela@ccc.edu.ph>
2025-05-29 03:12:41 +0800

    asdfasdf

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,14 @@ Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
 
+// NOTE ROUTES
 Route::apiResource('note', NoteController::class)
     ->middleware('auth:sanctum')
     ->middleware('can:modify,note')->only(['show', 'update', 'destroy']);
 
-// Then add the routes that don't need ownership check
-Route::get('/note', [NoteController::class, 'index'])->middleware('auth:sanctum');
-Route::post('/note', [NoteController::class, 'store'])->middleware('auth:sanctum');
+Route::get('/note', [NoteController::class, 'index'])
+    ->middleware('auth:sanctum');
+    
+Route::post('/note', [NoteController::class, 'store'])
+    ->middleware('auth:sanctum');
 
