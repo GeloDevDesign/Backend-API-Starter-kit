3dec9657b13b55374d7f7db812aab6f508c4e69d
GeloDevDesign <abserenuela@ccc.edu.ph>
2025-05-29 15:39:50 +0800

    asdfasdf

--- app/Policies/NotePolicy.php
+++ app/Policies/NotePolicy.php
@@ -9,8 +9,10 @@ use Illuminate\Auth\Access\Response;
 class NotePolicy
 {
 
-    public function modify(User $user, Note $note): Response
+    public function modify(User $user, Note $note): bool
     {
-        return $user->id === $note->user_id ? Response::allow() : Response::deny('You do not own this note');
+        return $user->id === $note->user_id;
+
+        //  return $user->id === $note->user_id ? Response::allow() : Response::deny('You do not own this note');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\AuthController;
 
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/register',[AuthController::class,'register']);
-Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // NOTE ROUTES
-Route::apiResource('note', NoteController::class)
-    ->middleware('auth:sanctum')
-    ->middleware('can:modify,note')->only(['show', 'update', 'destroy']);
 
-Route::get('/note', [NoteController::class, 'index'])
-    ->middleware('auth:sanctum');
-    
-Route::post('/note', [NoteController::class, 'store'])
-    ->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('note', NoteController::class)
+        ->middleware('can:modify,note')->only(['show', 'update', 'destroy']);
+
+    Route::apiResource('note', NoteController::class)->only(['index', 'show', 'store']);
+});
+
 
